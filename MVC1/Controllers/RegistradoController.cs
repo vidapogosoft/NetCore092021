8819fdd227df66f0192cee5ee5117c2a3bcffd48
vidapogosoft <vidapogosoft@hotmail.com>
2021-09-08 18:45:29 -0500

    demos for mvc
    call apis en mcv

---
+++ MVC1/Controllers/RegistradoController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MVC1.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MVC1.Controllers
+{
+    public class RegistradoController : Controller
+    {
+        string Baseurl = "http://localhost:51597/";
+
+        // GET: RegistradoController
+        public async Task<ActionResult> Index()
+        {
+            List<Registrado> empInfo = new List<Registrado>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage Res = await client.GetAsync("api/Mantenimiento/");
+                if (Res.IsSuccessStatusCode)
+                {
+                    var empResponse = Res.Content.ReadAsStringAsync().Result;
+                    empInfo = JsonConvert.DeserializeObject<List<Registrado>>(empResponse);
+                }
+
+                return View(empInfo);
+            }
+        }
+
+        // GET: RegistradoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RegistradoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegistradoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistradoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistradoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistradoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistradoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC1/Models/Registrado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC1.Models
+{
+    public class Registrado
+    {
+        public int IdRegistrado { get; set; }
+        public string Identificacion { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string NombresCompletos { get; set; }
+
+    }
+}
