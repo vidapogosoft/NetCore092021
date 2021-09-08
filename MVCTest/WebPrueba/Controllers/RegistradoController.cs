093eea94bbcb432ab65d377d86b1328c0058023f
jose.pluas <jose.pluas@deprati.com.ec>
2021-09-08 18:12:04 -0500

    Taller 2

---
+++ MVCTest/WebPrueba/Controllers/RegistradoController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using WebPrueba.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace WebPrueba.Controllers
+{
+    public class RegistradoController : Controller
+    {
+        string Baseurl = "http://localhost:40884/";
+
+        // GET: Registrado
+        public async Task<ActionResult> Index()
+        {
+            var listRegistrados = new List<Registrado>();
+            
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage Res = await client.GetAsync("api/Registrado/");
+                if (Res.IsSuccessStatusCode)
+                {
+                    var empResponse = Res.Content.ReadAsStringAsync().Result;
+                    listRegistrados = JsonConvert.DeserializeObject<List<Registrado>>(empResponse);
+                }
+            }
+
+            return View(listRegistrados);
+        }
+
+        // GET: Registrado/Details/5
+        public async Task<ActionResult> Details(string identificacion)
+        {
+            var registrados = new Registrado();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();                
+
+                HttpResponseMessage Res = await client.GetAsync("api/Registrado/" + identificacion);
+                if (Res.IsSuccessStatusCode)
+                {
+                    var empResponse = Res.Content.ReadAsStringAsync().Result;
+                    registrados = JsonConvert.DeserializeObject<List<Registrado>>(empResponse).FirstOrDefault();
+                }
+            }
+
+            return View(registrados);
+        }
+
+        // GET: Registrado/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Registrado/Create
+        [HttpPost]
+        public ActionResult Create(Registrado collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(Baseurl);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var json = JsonConvert.SerializeObject(collection);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage Res = client.PostAsync("api/Registrado", content).GetAwaiter().GetResult();
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        var empResponse = Res.Content.ReadAsStringAsync().Result;                        
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Registrado/Edit/5
+        public async Task<ActionResult> Edit(string identificacion)
+        {
+            var registrados = new Registrado();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+
+                HttpResponseMessage Res = await client.GetAsync("api/Registrado/" + identificacion);
+                if (Res.IsSuccessStatusCode)
+                {
+                    var empResponse = Res.Content.ReadAsStringAsync().Result;
+                    registrados = JsonConvert.DeserializeObject<List<Registrado>>(empResponse).FirstOrDefault();
+                }
+            }
+
+            return View(registrados);
+        }
+
+        // POST: Registrado/Edit/5
+        [HttpPost]
+        public ActionResult Edit(string id, Registrado collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(Baseurl);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var json = JsonConvert.SerializeObject(collection);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage Res = client.PutAsync("api/Registrado", content).GetAwaiter().GetResult();
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        var empResponse = Res.Content.ReadAsStringAsync().Result;
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Registrado/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Registrado/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
