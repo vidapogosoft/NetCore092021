1cc2fc1f8050bc5a47de6fc4d3166fa18ab9586c
vidapogosoft <vidapogosoft@hotmail.com>
2021-09-15 16:11:31 -0500

    swagger
    demo with swagger

---
+++ web-api-help-pages-using-swagger/samples/2.0/TodoApi.NSwag/Startup2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup2
+    {
+        #region snippet_ConfigureServices
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc();
+
+            #region snippet_AddSwaggerDocument
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "ToDo API";
+                    document.Info.Description = "A simple ASP.NET Core web API";
+                    document.Info.TermsOfService = "None";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = "https://twitter.com/spboyer"
+                    };
+                    document.Info.License = new NSwag.OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    };
+                };
+            });
+            #endregion snippet_AddSwaggerDocument
+        }
+        #endregion snippet_ConfigureServices
+
+        #region snippet_Configure
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
+            app.UseMvc();
+        }
+        #endregion snippet_Configure
+    }
+}
---
+++ web-api-help-pages-using-swagger/samples/2.0/TodoApi.Swashbuckle/Startup2.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+#region snippet_InfoClassNamespace
+using Microsoft.OpenApi.Models;
+#endregion
+using TodoApi.Models;
+#region snippet_PreReqNamespaces
+using System;
+using System.Reflection;
+using System.IO;
+#endregion
+namespace TodoApi
+{
+    public class Startup2
+    {
+        #region snippet_ConfigureServices
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+        }
+        #endregion
+
+        #region snippet_Configure
+        public void Configure(IApplicationBuilder app)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseMvc();
+        }
+        #endregion
+    }
+}
