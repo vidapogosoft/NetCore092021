7868335fd060522d9f841d992d9cf5e7fd0b9491
vidapogosoft <vidapogosoft@hotmail.com>
2021-09-22 17:41:23 -0500

    Controller de Customer
    Customer Api
    Customer Persistence
    Customer Model

---
+++ microservices/Catalogo.Services.Queries/ProductInStockQueryService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Catalogo.Persistence.Database;
+using Catalogo.Services.Queries.DTOs;
+using Microsoft.EntityFrameworkCore;
+using Services.Common.Collection;
+using Services.Common.Mapping;
+using Services.Common.Paging;
+
+
+
+namespace Catalogo.Services.Queries
+{
+    public interface IProductInStockQueryService
+    {
+        Task<DataCollection<ProductInStockDto>> GetAllAsync(int page, int take,
+            IEnumerable<int> products = null);
+    }
+
+    public class ProductInStockQueryService: IProductInStockQueryService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductInStockQueryService(
+            ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<DataCollection<ProductInStockDto>> GetAllAsync(int page, int take,
+            IEnumerable<int> products = null)
+        {
+            var collection = await _context.Stocks
+                .Where(x => products == null || products.Contains(x.ProductId))
+                .OrderBy(x => x.ProductId)
+                .GetPagedAsync(page, take);
+
+            return collection.MapTo<DataCollection<ProductInStockDto>>();
+
+        }
+
+    }
+}
---
+++ microservices/Customer.Persistence.Database/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Customer.Domain;
+using Microsoft.EntityFrameworkCore;
+using Customer.Persistence.Database.Configuration;
+
+namespace Customer.Persistence.Database
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options
+        )
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Database schema
+            builder.HasDefaultSchema("Customer");
+
+            // Model Contraints
+            ModelConfig(builder);
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+        private void ModelConfig(ModelBuilder modelBuilder)
+        {
+            new ClientConfiguration(modelBuilder.Entity<Client>());
+        }
+
+
+
+    }
+}
