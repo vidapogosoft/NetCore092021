572ed89f395eaccd80b6478362760fce1a8f61ad
vidapogosoft <vidapogosoft@hotmail.com>
2021-09-30 15:57:00 -0500

    Order Detail
    Proyecto con el proxy de detalle de la orden

--- microservices/Clients.WebClient/Pages/Orders/Detail.cshtml.cs
+++ microservices/Clients.WebClient/Pages/Orders/Detail.cshtml.cs
@@ -1,16 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Api.Gateway.WebClient.Proxy;
+using Api.Gateway.Models.Order.DTOs;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Clients.WebClient.Pages.Orders
 {
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        private readonly IOrderProxy _orderProxy;
+
+
+        public OrderDto Order { get; set; }
+
+        public DetailModel(
+             IOrderProxy orderProxy
+            )
         {
+            _orderProxy = orderProxy;
+        }
+
+        public async Task OnGet(int id)
+        {
+            Order = await _orderProxy.GetAsync(id);
         }
     }
 }
